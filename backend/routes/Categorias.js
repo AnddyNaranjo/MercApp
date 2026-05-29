c232d94feaeaea72b1f7d1d5c43955f12b827467
Anddy Naranjo <anddy.naranjo@example.com>
2026-05-29 11:01:06 -0500

    Fix: corregir capitalización de nombres en controllers

--- backend/routes/Categorias.js
+++ backend/routes/Categorias.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/categoriasController');
+const controller = require('../controllers/CategoriasController');
 
 
 // CREATE con imagen
